de352cdb426da9c65dc119ed74966e40371b76f4
vishw <vishw@HP>
2025-10-13 07:28:16 +0530

    Data types/mon13

---
+++ Trial1_practicing_python_code/basics/data_types.py
@@ -0,0 +1,20 @@
+'''
+Created on 13-Oct-2025
+
+@author: vishw
+
+Data types :
+type() : a predefined function to check the type of the value 
+key words : starts with cap letter
+Derived data types :
+type casting :
+
+'''
+num1 = 6
+print(type(num1))
+var1 = True
+var2 = False
+Var3 = None
+num2 = float(num1)
+print(num2)
+print(type(num2))
