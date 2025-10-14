09efe71c34d6f833459372a043acf853d69ae5fb
vishw <vishw@HP>
2025-10-14 18:16:08 +0530

    Type Conversion and some exceptional cases

--- Trial1_practicing_python_code/basics/np_data_types.py
+++ Trial1_practicing_python_code/basics/np_data_types.py
@@ -21,12 +21,21 @@ var3 = None #none type keyword is used if the value assigned to variable is yet 
 print( 'var3 data type ,var3 :',type(var3))
 
 #Type casting : converting one data type into other data type
+#int -> float
 num4 = float(num1)
 print(num4)
-print('After type casting :' ,type(num2))
+print('int->float,num4 :' ,num4 ,type(num2))
 
+# complex -> float
+#num11 = float(num3)
+#print('complex -> float ,num11 :',num11) #TypeError: float() argument must be a string or a real number, not 'complex'
+
+#float ->int
 num5 = int(num2)
-print('After type casting' ,num5)
+print('float -> int ,num5 :' ,num5)
+
+#num12 = int(num3)
+#print('complex -> int ,num12 :',num12)
 
 '''num10 =int(num3)
 print('num10 :',num10) #TypeError: int() argument must be a string, a bytes-like object or a real number, not 'complex'
@@ -39,6 +48,31 @@ print(type(num7))
 num8 = 0
 num9 =bool(num8)
 print('num9:', num9)
+
+name = "vishwa"
+print("Data type of 'name' :" ,type(name))
+
+#string -> int
+#myint = int(name)
+#print(" myint :",myint) ValueError: invalid literal for int() with base 10: 'vishwa'
+
+#string -> float
+#myfloat = float(name)
+#print(" myfloat :",myfloat) #ValueError: could not convert string to float: 'vishwa'
+
+#string -> boolean
+mybool = bool(name)
+print("mybool :",mybool)
+print("Data type of mybool :",type(mybool))
+
+#string -> int -> boolean 
+shape = "0"
+shape1 = int(shape)
+var4 = bool(shape1)
+print("var4 :",var4)
+print("Data type of var4 :",type(var4))
+
+
 
 
 
